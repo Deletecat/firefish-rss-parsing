d87ddcde36d229998f36f94367ec65ec43c555ad
Deletecat <55948500+Deletecat@users.noreply.github.com>
2023-09-10 11:41:48 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # firefish-rss-parsing
+
 This is intended to be used for my own site - deletecat.com - though you might find it useful!
+
+## Requirements
+
+- Python 3.11
+- Python3-feedparser
+- wget
+
+### Setup
+
+If you are using Debian, you can run the following commands to setup python to use this script:
+
+```
+sudo apt update && sudo apt upgrade
+sudo apt install python3 python3-feedparser wget
+```
+
+This may be different for the distribution you may be using.
+
+## What data does trhis script use?
+
+This script uses the following data from the rss feed:
+- feed['title'] - this contains the display name and the username of the firefish account
+- feed['link'] - this contains a link to the firefish account's page
+- feed['image']\['href'] - this contains a link to the firefish account's profile picture stored on the instance's CDN
+- entry['title'] - this contains the display name of the account and one of two words; 'says' or 'replies'
+- entry['link'] - this contains a link to the post shown in the entry
+- entry['published'] - this contains the date the post was published
+- entry['content']\['value'] - this contains the content stored in the entry
+
+## What can I change in this script to use it on my website?
+
+My code is a bit shit, but you can change the following variables:
+- url - replace with your account url / rss feed
+- stylesheet - replace with the name of your stylesheet
+
+The script will overwrite the previous `index.html` everytime it is run. It also downloads your profile picture from the cdn every time it is run. I am using an <iframe\> to display this on my site's home page without using up the instance's bandwidth. 
+
+The script ignores any replies and will also remove the <span\> tag from the end of `entry['content']['value']`.
+
+I intend to use this with a Github action's script to update whenever there is a new entry on the rss feed.
---
+++ firefish.py
@@ -0,0 +1,89 @@
+#__import modules
+import feedparser
+import os
+
+#__parse url
+url = "https://kitty.social/@deletecat/rss"
+feed = feedparser.parse(url)
+
+entry_data = [] #__array will store entry dictionaries
+entry_no = 0    #__variable will store number of entries
+entry_max = 6   #__variable will store max number of entries displayed
+
+username = feed.feed['title']
+userlink = feed.feed['link']
+userpfp = feed.feed.image['href']
+
+#__repeat for every entry in the feed
+for index in range(len(feed.entries)):
+    #__set the entry to be the current entry
+    entry = feed.entries[index]
+    #__if the number of recent entries is equal to the max, end the loop
+    if entry_no == entry_max:
+        break
+    #__split the title into an array
+    title = entry.title.split(" ")
+    #__if the last word in the title array == "says"
+    if title[len(title)-1] == "says":
+        entry_no += 1 #__add 1 to our number of entries
+        #__add the entry data to a temporary variable
+        entry_temp = {"entrylink": entry.link,
+                      "published":entry.published,
+                      "content":entry.content[0]['value']}
+        entry_data.append(entry_temp) #__add our temporary dictionary to our array
+
+#__split off the file extention
+pfpType = userpfp.split(".")
+#__if the file extension is gif, download the gif as 'pfp.gif'
+if pfpType[len(pfpType)-1] == "gif":
+    os.system(f"wget -O pfp.gif {userpfp}")
+    #__set userpfp to local file
+    userpfp = "pfp.gif"
+#__if the file extension is png, download the png as 'pfp.png'
+elif pfpType[len(pfpType)-1] == "png":
+    os.system(f"wget -O pfp.png {userpfp}")
+    userpfp = "pfp.png"
+#__if the file extension is jgp, download the jpg as 'pfp.jpg'
+elif pfpType[len(pfpType)-1] == "jpg":
+    os.system(f"wget -O pfp.jpg {userpfp}")
+    userpfp = "pfp.jpg"
+
+"""
+Now that the data has been read into the array, 
+and that we have our profile picture downloaded,
+it's time to move onto making our html page.
+"""
+stylesheet = "style.css"
+#__set initial lines of html
+html_lines = f"""<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{stylesheet}">
+    <title>RSS Feed</title>
+</head>
+<body>
+"""
+
+#__repeat for every entry in the feed
+for index in range(len(entry_data)):
+    #__repeat for every letter in the content of the entry
+    for contIndex in range(len(entry_data[index]['content'])):
+        #__removes <span> from the output
+        if entry_data[index]['content'][contIndex] == "<" and entry_data[index]['content'][contIndex+1] == "s":
+            content = entry_data[index]['content'][:contIndex-1]
+            break #__the loop no longer has to continue
+    #__replaces new lines with <br> tags
+    content=content.replace("\n","<br>")
+    #__add entry data to page
+    html_lines += f'''<img src="{userpfp}"><h3><a href="{userlink}">{username}</a></h3>
+    <h5>{entry_data[index]["published"]}</h5>
+    <p>{content}</p>
+    <span><a href="{entry_data[index]["entrylink"]}">Link to post</a></span><hr>\n'''
+#__end page
+html_lines += "</body>\n</html>"
+#__open the file in write mode, this will create the file if it doesn't already exist
+with open("index.html","w") as writer:
+    #__write the page to file
+    writer.write(html_lines)
